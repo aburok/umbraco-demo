6aa5a270ada1aae0210e26fb3e55bf9bb8435a7f
dkoruba <dawid.koruba@thecogworks.com>
2016-06-03 12:32:18 +0200

    surface controller , validator + views

---
+++ UmbracoDemo/Controllers/ValidationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using System.Web.UI;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using UmbracoTest.Validation;
+
+namespace UmbracoTest.Controllers
+{
+    [OutputCache(Location = OutputCacheLocation.None, NoStore = true)]
+    public class ValidationController : SurfaceController
+    {
+        [System.Web.Http.HttpPost]
+        public JsonResult ValidatePassword(ValidatePasswordRequest request)
+        {
+            // both are required
+            if (string.IsNullOrWhiteSpace(request.EmailAddress)
+                || string.IsNullOrWhiteSpace(request.UserName)
+                || string.IsNullOrWhiteSpace(request.Password))
+            {
+                return Json(true);
+            }
+
+            var validationResults = PasswordValidator.Validate(
+                request.Password,
+                request.UserName,
+                request.EmailAddress);
+
+            // if no errors were returned then return true
+            if (validationResults == PasswordMessage.Valid)
+                return Json(true);
+
+            return Json(validationResults.ToString());
+        }
+    }
+
+    public class ValidatePasswordRequest
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ UmbracoDemo/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace UmbracoTest.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [DisplayName("User name")]
+        [Required]
+        public string UserName { get; set; }
+
+        [DisplayName("Email address")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Remote("ValidatePassword", "Validation", HttpMethod = "POST", AdditionalFields = "EmailAddress, UserName")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Password confirmation")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+    }
+}
